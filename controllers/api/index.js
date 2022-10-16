6228126e6d34678f28b1bb7cb2979870d77bc724
Fabian Gutierrez <fabiangutierrez580@gmail.com>
2022-10-16 13:38:10 -0500

    recipient post route working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const giftRoutes = require('./giftRoutes');
+const recipientRoutes = require('./recipientRoutes');
 
 router.use('/users', userRoutes);
 router.use('/gift', giftRoutes);
+router.use('/recipient', recipientRoutes)
 
 module.exports = router;
--- public/js/profile.js
+++ public/js/profile.js
@@ -23,6 +23,32 @@ const newFormHandler = async (event) => {
   }
 };
 
+const recipientHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#recipient-name').value.trim();
+  const relationship = document.querySelector('#recipient-relationship').value.trim();
+  const budget = document.querySelector('#budget').value.trim();
+  const hasGiftIdea = document.querySelector("#hasGiftIdea").checked
+
+  if (name && relationship && budget && hasGiftIdea ) {
+    const response = await fetch(`/api/recipient`, {
+      method: 'POST',
+      body: JSON.stringify({ name, relationship, budget, hasGiftIdea }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/gift');
+    } else {
+      alert('Failed to create recipient');
+    }
+  }
+}
+
+
 const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
@@ -38,11 +64,15 @@ const delButtonHandler = async (event) => {
     }
   }
 };
+document
+.querySelector('#addRecipientButton')
+.addEventListener('click', recipientHandler);
 
 document
   .querySelector('#gift-btn')
   .addEventListener('click', newFormHandler);
 
-document
-  .querySelector('.gift-list')
-  .addEventListener('click', delButtonHandler);
+
+// document
+//   .querySelector('.gift-list')
+//   .addEventListener('click', delButtonHandler);
