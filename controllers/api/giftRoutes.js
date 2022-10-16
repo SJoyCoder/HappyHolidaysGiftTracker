8c514a178363f4a92141b25a9b59d1cc4e0ebf7a
Fabian Gutierrez <fabiangutierrez580@gmail.com>
2022-10-16 12:11:32 -0500

    recipient route

--- controllers/api/giftRoutes.js
+++ controllers/api/giftRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Gifts, RecipientGifts } = require('../../models');
+const { Gifts, RecipientGifts, Recipient} = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
